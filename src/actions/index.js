89b7f5fb9e3874647796fee589e400a51af7fe7e
Kadir Uysaloglu <31732925+KadirUysaloglu@users.noreply.github.com>
2018-05-18 10:14:59 +0100

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,6 @@ export const GAME_SELECTED='game_selected';
 export const DELETE_GAME='game_delete';
 
 const URL = 'https://zyqh9s9xt4.execute-api.eu-west-1.amazonaws.com/prod/battle';
-const config = {headers: {'x-api-key': 'x79qTUx0QO5IzR8zsiFvt5a5xOy5HikV2QBLjy0D'}};
 
 
 export function fetchGames() {
